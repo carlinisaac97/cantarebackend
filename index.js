d518aec5dbdedd896fd0c3510473151619325add
Isaac <carlin_isaac@hotmail.com>
2022-04-07 23:10:14 -0400

    first_add

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "upload/" });
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
+
+require("./src/routes/usuario/usuario.routes")(app);
+require("./src/routes/canciones/canciones.routes")(app);
+require("./src/routes/pedido/pedido.routes")(app);
+require("./src/routes/artista/artista.routes")(app);
+require("./src/routes/video/video.routes")(app);
+
+
+app.listen(3000, () => console.log("listening on port 3000"));
