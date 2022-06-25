c7e0a48f723cd40778e8806d5c3bc471dda60579
Isaac <carlin_isaac@hotmail.com>
2022-06-24 20:09:49 -0400

    add tabla

---
+++ archivos2Final/roles.sql
@@ -0,0 +1,20 @@
+INSERT INTO public.roles (rol_fecha,rol_descripcion,rol_usuario_id) VALUES
+	 ('2023-12-06','Administrador 1',1),
+	 ('2023-12-06','Administrador 1',1),
+	 ('2022-06-24','Administrador 1',1),
+	 ('2022-06-24','Administrador 1',1),
+	 ('2022-06-24','Administrador 1',1),
+	 ('2022-06-24','Administrador 1',1),
+	 ('2022-06-24','Administrador 1',1),
+	 ('2022-06-24','Administrador 1',1),
+	 ('2022-06-24','Prueba 1',1),
+	 ('2022-06-24','Prueba 2',1);
+INSERT INTO public.roles (rol_fecha,rol_descripcion,rol_usuario_id) VALUES
+	 ('2022-06-24','Prueba 3',1),
+	 ('2022-06-24','Prueba 4',1),
+	 ('2022-06-24','Prueba 5',1),
+	 ('2022-06-24','Prueba 6',1),
+	 ('2022-06-24','Prueba 7',1),
+	 ('2022-06-24','Prueba 8',1),
+	 ('2022-06-24','Prueba 9',1),
+	 ('2022-06-24','Prueba 10',1);
